232b1925533921c0090f92d03cb01e02ad15e845
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-02-22 14:02:05 -0300

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/ask', to: 'questions#ask'
+  get '/questions', to: 'questions#answer'
 end
+
+
+
+
+
